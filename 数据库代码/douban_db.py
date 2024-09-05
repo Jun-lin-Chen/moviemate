7b0a46a20c9ae4e3fe637f1a3d6dde6745108eab
盛浅予 <14814044+sheng-qianyu@user.noreply.gitee.com>
2024-09-05 00:33:48 +0000

    豆瓣电影数据的数据库管理
    Signed-off-by: 盛浅予 <14814044+sheng-qianyu@user.noreply.gitee.com>

--- 数据库代码/douban_db.py
+++ 数据库代码/douban_db.py
@@ -4,16 +4,6 @@
 import pymysql
 import pandas as pd
 
-# 连接数据库
-def database_connection(host, user, password, port, database, charset):
-    connection = None
-    try:
-        connection = pymysql.connect(host=host, user=user, password=password, port=port, db=database, charset=charset)
-        return connection
-    except Exception as e:
-        print('在连接数据库时发生异常：', e)
-        raise  # 重新抛出异常
-
 # 创建数据库
 def create_database(cursor, database):
     try:
@@ -28,8 +18,16 @@ def create_douban_table(cursor):
     try:
         sql = '''
         CREATE TABLE IF NOT EXISTS douban_movies (
+            director VARCHAR(255) NOT NULL,
+            starring VARCHAR(255) NOT NULL,
+            genre VARCHAR(255) NOT NULL,
+            region VARCHAR(255) NOT NULL,
+            year VARCHAR(255) NOT NULL,
+            detail_url VARCHAR(255) NOT NULL,
             title VARCHAR(255) NOT NULL,
-            douban_grade FLOAT
+            rating VARCHAR(255) NOT NULL,
+            rating_count VARCHAR(255) NOT NULL,
+            poster_url VARCHAR(255) NOT NULL
         )
         '''
         cursor.execute(sql)
@@ -40,27 +38,12 @@ def create_douban_table(cursor):
 
 def add_all_douban_data(cursor, data):
     try:
-        sql = 'INSERT IGNORE INTO douban_movies(title, douban_grade) VALUES(%s, %s)'
+        sql = 'INSERT IGNORE INTO douban_movies(director, starring, genre, region, year, detail_url, title, rating, rating_count, poster_url) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
         cursor.executemany(sql, data)
         cursor.connection.commit()
         print(f"成功添加 {cursor.rowcount} 条数据。")
     except Exception as e:
         print('添加所有数据时发生异常：', e)
-        raise
-
-def add_single_douban_data(cursor, title, douban_grade):
-    try:
-        sql = '''
-        INSERT INTO douban_movies (title, douban_grade)
-        VALUES (%s, %s)
-        ON DUPLICATE KEY UPDATE
-        douban_grade = VALUES(douban_grade);
-        '''
-        cursor.execute(sql, (title, douban_grade))
-        cursor.connection.commit()
-        print("单个数据添加完成。")
-    except Exception as e:
-        print('向数据库添加单个电影评分数据时发生异常：', e)
         raise
 
 def delete_all_douban_data(cursor):
@@ -71,36 +54,6 @@ def delete_all_douban_data(cursor):
     except Exception as e:
         print('删除所有数据时发生异常：', e)
         raise
-
-# 通过电影名称查找并删除单条数据
-def delete_single_douban_data_by_title(cursor, title):
-    result = cursor.fetchone()
-    if result:
-        try:
-            sql = 'DELETE FROM douban_movies WHERE title = %s'
-            cursor.execute(sql, (title,))
-            cursor.connection.commit()
-            print(f"已删除标题为 '{title}' 的数据。")
-        except Exception as e:
-            print('从数据库删除电影评分数据时发生异常：', e)
-            raise
-    else:
-        print("未找到匹配的数据。")
-
-# 通过电影名称查找并更新单条数据
-def update_single_douban_data_by_title(cursor, title, new_grade):
-    result = cursor.fetchone()
-    if result:
-        try:
-            sql = 'UPDATE douban_movies SET douban_grade = %s WHERE title = %s'
-            cursor.execute(sql, (new_grade, title))
-            cursor.connection.commit()
-            print(f"已更新标题为 '{title}' 的电影评分。")
-        except Exception as e:
-            print('更新数据库电影评分数据时发生异常：', e)
-            raise
-    else:
-        print("未找到匹配的数据。")
 
 # 通过电影名称查找单条数据
 def search_single_douban_data_by_title(cursor, title):
@@ -117,12 +70,24 @@ def search_single_douban_data_by_title(cursor, title):
         print('查询数据库电影评分数据时发生异常：', e)
         raise
 
-# 读取csv文件
-def read_csv(file_path):
+# # 读取csv文件
+# def read_csv(file_path):
+#     try:
+#         return pd.read_csv(file_path, dtype=str).values.tolist()
+#     except Exception as e:
+#         print('读取CSV文件时发生异常：', e)
+#         raise
+
+def read_excel(file_path):
     try:
-        return pd.read_csv(file_path, dtype=str).values.tolist()
+        # 读取 Excel 文件
+        df = pd.read_excel(file_path, dtype=str)
+        # 替换 NaN 值为 ''
+        df.fillna('', inplace=True)
+        # 将 DataFrame 转换为列表
+        return df.values.tolist()
     except Exception as e:
-        print('读取CSV文件时发生异常：', e)
+        print('读取Excel文件时发生异常：', e)
         raise
 
 def main():
@@ -141,27 +106,14 @@ def main():
 
             create_douban_table(cursor)
 
-            file_path = r'../爬取网站代码/douban_top250.csv'
-            data = read_csv(file_path)
+            file_path = r'../爬取网站代码/豆瓣电影.xlsx'
+            data = read_excel(file_path)
 
-            delete_all_douban_data(cursor)
+            print('数据长度', len(data))
+
+            #delete_all_douban_data(cursor)
             add_all_douban_data(cursor, data)
             print("数据添加完成。")
-
-            print("准备删除数据...")
-            delete_movie_title = input('请输入要删除的电影的名称：')
-            if delete_movie_title.strip():
-                delete_single_douban_data_by_title(cursor, delete_movie_title)
-            else:
-                print("未输入有效的电影名称。")
-
-            print("准备更新数据...")
-            update_movie_title = input('请输入要更新的电影的名称：')
-            if update_movie_title.strip():
-                update_movie_grade = 9.9
-                update_single_douban_data_by_title(cursor, update_movie_title, update_movie_grade)
-            else:
-                print("未输入有效的电影名称。")
 
             print("准备查询数据...")
             search_movie_title = input('请输入要查询的电影的名称：')
