dd7aa0efd60abcf4f7860138a73620d2934e0ece
Yukun Tian <213221787@seu.edu.cn>
2024-09-08 21:47:43 +0800

    search debug

--- GUI/gui/webGUI/views.py
+++ GUI/gui/webGUI/views.py
@@ -1,58 +1,18 @@
-import pymysql, json
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from django.shortcuts import HttpResponse  # 导入HttpResponse模块
 from .models import UserInfo
+import json, pymysql
 
 def helloworld(request):  # request是必须带的实例。类似class下方法必须带self一样
     return HttpResponse("Hello World!!")  # 通过HttpResponse模块直接返回字符串到前端页面
 
 def index_view(request):
+    question = request.POST.get('search', '')
+    print(f'question: {question}')
+
     return render(request, 'index.html')
-# def post_view(request):
-#     return render(request, 'post.html')
-def author_view(request):
-    return render(request, 'author.html')
-
-#登录界面
-def toLogin_view(request):
-    return render(request, 'login.html') #返回渲染好的login界面
-
-#将用户输入的用户名与密码与数据库中数据进行比对
-def Login_view(request):
-
-    u = request.POST.get('user','')
-    p = request.POST.get('password','')
-
-    if u and p :
-        c = UserInfo.objects.filter(user_name=u, user_password=p).count()
-        if c:
-            return HttpResponseRedirect('http://127.0.0.1:8000/dev/index/')
-        else:
-            return HttpResponse('Login failed！')
-    else:
-        return HttpResponse('Your user name or password should not be empty！')
-
-#注册界面
-def toRegister_view(request):
-    return render(request, 'register.html') #返回渲染好的界面
-
-#将用户注册信息存入数据库
-def register_view(request):
-    u = request.POST.get('user', '')
-    p = request.POST.get('password', '')
-    if u and p:
-        if UserInfo.objects.filter(user_name=u).exists():
-            return HttpResponse('This username is already taken. Please try another one.')
-        else:
-            stu = UserInfo(user_name=u, user_password=p)
-            stu.save()
-            # Pass a message to the template
-            message = 'Register successfully! Redirecting to login page in 2s...'
-            return render(request, 'register_success.html', {'message': message})
-    else:
-        return HttpResponse('Please try again！')
 
 def search_single_douban_data_by_title(cursor, title):
     try:
@@ -75,8 +35,8 @@ def search_single_douban_data_by_title(cursor, title):
 
 def post_view(request):
     # 搜索的电影
-    # question = request.GET.get('search-item', '')
-    question = '少年的你'
+    question = request.GET.get('search-item', '') # 有问题！
+    # question = '少年的你'
     print(f'question: {question}')
     host = 'localhost'
     user = 'root'
@@ -125,4 +85,46 @@ def post_view(request):
                 connection.close()  # 确保在最后关闭数据库连接
             return response
     else:
-        return HttpResponse('No input empty search')
+        return render(request, 'post.html')
+
+def author_view(request):
+    return render(request, 'author.html')
+
+#登录界面
+def toLogin_view(request):
+    return render(request, 'login.html') #返回渲染好的login界面
+
+#将用户输入的用户名与密码与数据库中数据进行比对
+def Login_view(request):
+
+    u = request.POST.get('user','')
+    p = request.POST.get('password','')
+
+    if u and p :
+        c = UserInfo.objects.filter(user_name=u, user_password=p).count()
+        if c:
+            return HttpResponseRedirect('http://127.0.0.1:8000/dev/index/')
+        else:
+            return HttpResponse('Login failed！')
+    else:
+        return HttpResponse('Your user name or password should not be empty！')
+
+#注册界面
+def toRegister_view(request):
+    return render(request, 'register.html') #返回渲染好的界面
+
+#将用户注册信息存入数据库
+def register_view(request):
+    u = request.POST.get('user', '')
+    p = request.POST.get('password', '')
+    if u and p:
+        if UserInfo.objects.filter(user_name=u).exists():
+            return HttpResponse('This username is already taken. Please try another one.')
+        else:
+            stu = UserInfo(user_name=u, user_password=p)
+            stu.save()
+            # Pass a message to the template
+            message = 'Register successfully! Redirecting to login page in 2s...'
+            return render(request, 'register_success.html', {'message': message})
+    else:
+        return HttpResponse('Please try again！')
